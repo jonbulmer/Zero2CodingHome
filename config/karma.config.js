9c53545453cf30d83ec399bb8a01b578d0667a42
Jon Bulmer <jon.bulmer@gmail.com>
2018-06-07 19:42:46 +0100

    config file added

---
+++ config/karma.config.js
@@ -0,0 +1,36 @@
+var webpackConfig = require('./webpack.test');
+
+module.exports = function (config) {
+    var _config = {
+        basePath: '',
+
+        frameworks: ['jasmine'],
+
+        files: [
+            {pattern: './config/karma-test-shim.js', watched: false}
+        ],
+
+        preprocessors: {
+            './config/karma-test-shim.js': ['webpack', 'sourcemap']
+        },
+
+        webpack: webpackConfig,
+
+        webPackMiddleware: {
+            stats: 'error-only'
+        },
+
+        webServer: {
+            noInfo: true
+        },
+
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['PhantomJS'],
+        singleRun: true
+    };
+    config.set(_config);
+};
