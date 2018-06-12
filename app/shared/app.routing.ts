28b0bd9564579b7f0297e679041ca1aaaa66c7cd
Jon Bulmer <jon.bulmer@gmail.com>
2018-06-12 18:30:57 +0100

    app start

--- app/shared/app.routing.ts
+++ app/shared/app.routing.ts
@@ -4,15 +4,13 @@ import { RouterModule } from '@angular/router';
 import{ HomeComponent } from '../home/home.component';
 import{ ErrorComponent } from '../error/error.component';
 import { Lesson1Component } from "../lesson1/lesson1.component";
-import { CompanyModule } from '../companies/company.module' ;
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: '', component: HomeComponent },
             { path: '**' , component: ErrorComponent}
-        ]),
-        CompanyModule
+        ])
     ],
     exports: [
         RouterModule
