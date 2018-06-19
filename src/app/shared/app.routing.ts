a527db85d789be88fe2fbaf7d234815056c07bcd
Jon Bulmer <jon.bulmer@gmail.com>
2018-06-19 06:55:41 +0100

    added app routing

---
+++ src/app/shared/app.routing.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import{ HomeComponent } from '../home/home.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent }
+        ])
+    ],
+    exports: [
+        RouterModule
+    ]   
+})
+export class AppRoutingModule {}
