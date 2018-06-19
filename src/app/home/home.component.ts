ac3b3f115858edc50f7703cd7c8fe9dc600a28ba
Jon Bulmer <jon.bulmer@gmail.com>
2018-06-19 07:08:10 +0100

    First build that works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './home.component.html' //,
-    //styleUrls: ['./home.component.css']
+    templateUrl: './home.component.html'
 })
 export class HomeComponent {}
