056e18b673bd2a50587e34f42bc5ab4bd4685dc2
Jon Bulmer <jon.bulmer@gmail.com>
2018-07-19 19:28:15 +0100

    added images and lesson6

---
+++ src/app/lesson6/lesson6.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './lesson6.component.html',
+    styleUrls: ['./lesson6.component.css']
+})
+export class Lesson6Component {}
