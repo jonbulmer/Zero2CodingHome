2b1c0e528b7e9b5a1e70bbe5da54bad43c0fc0e5
Jon Bulmer <jon.bulmer@gmail.com>
2018-07-21 10:14:42 +0100

    Added the top bar

---
+++ src/app/shared/topbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'top-bar',
+    templateUrl:'./topbar.component.html',
+    styleUrls: ['./topbar.component.css']
+})
+export class TopbarComponent {}
