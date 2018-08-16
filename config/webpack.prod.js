302b925366444dc3686b5a69219cfd9f6f8cfbf8
Jon Bulmer <jon.bulmer@gmail.com>
2018-08-16 19:17:37 +0100

    lesson 3 work and created a build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -16,9 +16,9 @@ module.exports = webpackMerge(commonConfig, {
         chunkFilename: '[id].[hash].chunk.js'
     },
 
-    htmlLoader: {
-        minimize: false // workaround for ng2
-    },
+//    htmlLoader: {
+//        minimize: false // workaround for ng2
+//    },
 
     plugins: [
         new webpack.NoErrorsPlugin(),
