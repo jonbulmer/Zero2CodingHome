13aeb2023cbf66a90f20d264d6fa9fa1876cea39
Jon Bulmer <jon.bulmer@gmail.com>
2018-09-12 20:16:19 +0100

    test work

---
+++ Coding Tests/ChickenAndEgg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ChickenEgg
+{
+
+    public interface IBird
+    {
+        Egg Lay();
+    }
+    // Should implement IBird
+    public class Chicken : IBird
+    {
+        
+        public Egg Lay()
+        {
+            // pass the new chicken in the constructor and return it
+            var egg = new Egg(() => new Chicken());
+            return egg;
+        }
+    }
+    public class Egg
+    {
+        private readonly Func<IBird> _createBird;
+        bool _hasHatched;
+        public Egg(Func<IBird> createBird)
+        {
+            _createBird = createBird;
+        }
+
+        public IBird Hatch()
+        {
+            if (_hasHatched)
+            {
+                // this throws when we already have a child chicken and exits the function.
+                throw new NotImplementedException("The spec asks for only one child chicken per chicken.");
+            }
+            // set that we have a child chicken and return the chicken created in the constuctor
+            _hasHatched = true;
+            return _createBird();
+        }
+    }
+}
