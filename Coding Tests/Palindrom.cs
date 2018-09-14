82a0c8400091dab459033719cdcb57f52afe54ce
Jon Bulmer <jon.bulmer@gmail.com>
2018-09-14 18:33:42 +0100

    add the Palindrom code

---
+++ Coding Tests/Palindrom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralProblems
+{
+    public class Palindrom
+    {
+        public static bool PalindromCheck(string wordToCheck)
+        {
+            wordToCheck = wordToCheck.ToLower();
+            int halfWordCount = (int)Math.Floor(wordToCheck.Length / 2f);
+            for (int i = 0; i < halfWordCount; i++)
+            {
+                char firstLetter = wordToCheck[i];
+                char lastLetter = wordToCheck[wordToCheck.Length - (1+i)];
+                // check the first + i and last -i are equals
+                if (firstLetter != lastLetter)
+                {
+                    return false;
+                } 
+            }
+            return true;
+        }
+    }
+}
