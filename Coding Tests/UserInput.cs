93e9a725b6d39cbb71a6e6be3b54aa58e14e2b8f
Jon Bulmer <jon.bulmer@gmail.com>
2018-09-21 20:43:53 +0100

    Text input to demonstrate override functions using inheritance

---
+++ Coding Tests/UserInput.cs
@@ -0,0 +1,67 @@
+﻿/*
+User interface contains two types of user input controls: TextInput, which accepts all characters and NumericInput, 
+which accepts only digits.
+
+Implement the class TextInput that contains:
+
+Public method void Add(char c) - adds the given character to the current value
+Public method string GetValue() - returns the current value
+Implement the class NumericInput that:
+
+Inherits TextInput
+Overrides the Add method so that each non-numeric character is ignored
+For example, the following code should output "10":
+*/
+
+
+
+using System;
+
+namespace GeneralProblems
+{
+
+    public class TextInput
+    {
+        // this is the string where we store the output
+        protected string currentValue;
+        
+
+        public virtual void Add(char c)
+        {
+            currentValue += c.ToString();
+        }
+
+        public string GetValue()
+        {
+            // return the value entered into the 
+            return currentValue;
+        }
+    }
+
+    public class NumericInput : TextInput
+    {
+        public override void Add(char c)
+        {
+            if(c >= '0' && c <= '9')
+            {
+                currentValue += c.ToString();
+            }
+        }
+    }
+    public class UserInput
+    {
+    }
+}
+/*
+
+/*
+    public static void Main(string[] args)
+    {
+        //TextInput input = new NumericInput();
+        //input.Add('1');
+        //input.Add('a');
+        //input.Add('0');
+        //Console.WriteLine(input.GetValue());
+    }
+}
+*/
