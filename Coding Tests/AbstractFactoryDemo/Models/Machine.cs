9006d150cb09e358ddb26701132533faeac40e70
Jon Bulmer <jon.bulmer@gmail.com>
2018-10-22 07:27:31 +0100

    added code to the machine class

--- Coding Tests/AbstractFactoryDemo/Models/Machine.cs
+++ Coding Tests/AbstractFactoryDemo/Models/Machine.cs
@@ -4,9 +4,23 @@ using AbstractFactoryDemo.Interfaces;
 
 namespace AbstractFactoryDemo.Models
 {
-    class Machine
+    class Machine : IUser
     {
         public Producer Producer { get; set; }
         public string Model { get; set; }
+
+        public void SetIdentity(IUserIdentiy identity)
+        {
+            // identity must by MacAddress
+            // access identity.NicPart
+        }
+
+        public Machine(Producer producer, string model)
+        {
+            if (producer == null)
+            {
+                throw new AggregateException(); 
+            }
+        }
     }
 }
