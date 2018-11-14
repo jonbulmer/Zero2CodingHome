8516019bc3fdb87e272b179df2538db7bd29fc00
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-14 07:41:48 +0000

    Managing Data Integrity in EF

---
+++ Coding Tests/70 483/70-843/3.1/3.1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Exam.Objective3_1
+{
+    class ManagingDataIntegrity
+    {
+        public class Customer
+        {
+            public int Id { get; set; }
+
+            [Required, MaxLength(20)]
+            public string Firstname { get; set; }
+
+            [Required, MaxLength(20)]
+            public string LastName { get; set; }
+
+            [Required]
+            public Address ShippingAdderss { get; set; }
+
+            [Required]
+            public Address BillingAddress { get; set; }
+        }
+
+        public class Address
+        {
+            public int Id { get; set; }
+
+            [Required, MaxLength(20)]
+            public string  Addressline { get; set; }
+        }
+    }
+}
