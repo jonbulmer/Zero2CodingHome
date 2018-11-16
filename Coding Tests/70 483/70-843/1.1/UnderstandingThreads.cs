0e4ede48f061ebe25a88efa518fe20772fe94df4
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-16 07:31:03 +0000

    Understanding Threads and Parallel tasks

---
+++ Coding Tests/70 483/70-843/1.1/UnderstandingThreads.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Exam.Objective1_1
+{
+    public static class UsingThreads
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void CallThreadMethod()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+        }
+    }
+}
---
+++ Coding Tests/70 483/70-843/1.1/UsingParallel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exam.Objective1_1
+{
+    class UsingParallel
+    {
+        public static void ParallelFor()
+        {
+            Parallel.For(0, 10, i =>
+            {
+                Thread.Sleep(1000);
+            });
+        }
+
+        public static void ParallelBreak()
+        {
+            ParallelLoopResult result = Parallel.
+                For(0, 1000, (int i, ParallelLoopState
+                    loopState) =>
+                {
+                    if (i == 500)
+                    {
+                        Console.WriteLine("Breaking loop");
+                        loopState.Break();
+                    }
+                return;
+                });
+        }
+    }
+}
