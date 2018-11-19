3e96f1e4a56c20013c4ff9dd579e1392848f8bca
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-19 07:22:31 +0000

    Enum and struct

---
+++ Coding Tests/70 483/70-843/2.1/CreateTypes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective2_1
+{
+    public static class ChoosingTypeToCreate
+    {
+        public static class ImportanceEnum
+        {
+            enum Importance
+            {
+                None,
+                Trivial,
+                Regular,
+                Important,
+                Critical
+            }
+
+            public static void EnumMain()
+            {
+                // ... An enum local variable.
+
+                Importance value = Importance.Critical;
+
+                //
+                if (value == Importance.Trivial)
+                {
+                    Console.WriteLine($"You can do this later as it is {value} ");
+                }
+                else if (value == Importance.Critical)
+                {
+                    Console.WriteLine($"You must do this now as it is {value} ");
+                }
+            }
+
+            public struct Point
+            {
+                public int x, y;
+
+                public Point(int p1, int p2)
+                {
+                    x = p1;
+                    y = p2;
+                }
+            }
+        }
+    }
+}
