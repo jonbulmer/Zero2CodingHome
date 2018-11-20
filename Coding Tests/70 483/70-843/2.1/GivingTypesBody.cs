85589b8696899bb23b8de48b34fd62a52b6d6ee8
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-20 08:06:38 +0000

    Giving Types Body

---
+++ Coding Tests/70 483/70-843/2.1/GivingTypesBody.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective2_1
+{
+    public static class GivingTypesBody
+    {
+        public static void CallHelloWorld()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
