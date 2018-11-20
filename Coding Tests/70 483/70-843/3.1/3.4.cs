b4054a5dd4761ed8c5264dfbc3a846e290098bd5
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-20 19:43:47 +0000

    Debug applications

---
+++ Coding Tests/70 483/70-843/3.1/3.4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Exam.Objective3_4
+{
+    /// <summary>
+    /// This code demonstrates the difference on how Debug and relise 
+    /// handle a build.
+    /// In debug mode the Timer fires every 2 mins
+    /// In relise mode the complier will only once.
+    /// </summary>
+    class DebugAnApplication
+    {
+        public static void MainCall()
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 2000);
+            Console.ReadLine();
+        }
+
+        public static void TimerCallback(Object o)
+        {
+            Console.WriteLine("In TimerCallback: " + 
+                DateTime.Now);
+            GC.Collect();
+        }
+    }
+}
