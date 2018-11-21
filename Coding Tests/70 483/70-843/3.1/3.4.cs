9d3a8de7435976572ffc31b974b2de2b990ee2f3
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-21 07:52:52 +0000

    Debug symbols

--- Coding Tests/70 483/70-843/3.1/3.4.cs
+++ Coding Tests/70 483/70-843/3.1/3.4.cs
@@ -1,27 +1,61 @@
-﻿using System;
+﻿#define MySymbol
+using System;
+using System.Reflection;
 using System.Threading;
 
 namespace Exam.Objective3_4
 {
-    /// <summary>
-    /// This code demonstrates the difference on how Debug and relise 
-    /// handle a build.
-    /// In debug mode the Timer fires every 2 mins
-    /// In relise mode the complier will only once.
-    /// </summary>
+
     class DebugAnApplication
     {
+
+        public Assembly LoadAssembly<T>()
+        {
+#if !WINRT
+            Assembly assembly = typeof(T).Assembly;
+#else
+            Assembly assembly = typeof(T).GetTypeInfo().Assembly;   
+#endif
+
+        return assembly;
+        }
+
+        public static void UseCustomSymbol()
+        {
+#if MySymbol
+            Console.WriteLine("Custom symbol is defined");
+#endif
+        }
+
         public static void MainCall()
         {
             Timer t = new Timer(TimerCallback, null, 0, 2000);
             Console.ReadLine();
         }
 
+
+        /// <summary>
+        /// This code demonstrates the difference on how Debug and relise 
+        /// handle a build.
+        /// In debug mode the Timer fires every 2 mins
+        /// In relise mode the complier will only once.
+        /// </summary>
         public static void TimerCallback(Object o)
         {
             Console.WriteLine("In TimerCallback: " + 
                 DateTime.Now);
             GC.Collect();
         }
+
+        public void DebugDirective()
+        {
+#if DEBUG
+            Console.WriteLine("Debug mode");
+#else
+            Console.WriteLine("Not Debug");
+#endif
+        }
+
+
     }
 }
