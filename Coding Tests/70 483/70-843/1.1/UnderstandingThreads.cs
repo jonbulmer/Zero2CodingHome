43d196e67ebd90ed939ea8b9699705e13197d916
Jon Bulmer <jon.bulmer@gmail.com>
2018-11-30 14:06:06 +0000

    Add WaitAll and WaitAny

--- Coding Tests/70 483/70-843/1.1/UnderstandingThreads.cs
+++ Coding Tests/70 483/70-843/1.1/UnderstandingThreads.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -239,6 +240,51 @@ namespace Exam.Objective1_1
                     });
 
                 finalTask.Wait();
+            }
+        }
+
+        // Listing 1-14 Using Task.WaitAll
+        public static void UsingTaskWaitAll()
+        {
+            Task[] tasks = new Task[3];
+
+            tasks[0] =  Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("1");
+                    return 1;
+                });
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 1;
+            });
+            tasks[3] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+                return 1;
+            });
+            Task.WaitAll();
+        }
+
+        // Listing 1-15 Using Task.WaitAny
+        public static void UsingTaskWaitAny()
+        {
+            Task<int>[] tasks = new Task<int>[3];
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(2000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(2000); return 3; });
+
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+                Console.WriteLine(completedTask.Result);
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
             }
         }
     }
