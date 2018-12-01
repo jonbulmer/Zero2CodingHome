bb2b106428617f053bc3e193e6f003c7475b25c4
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-01 11:17:01 +0000

    more updates to threads

---
+++ Coding Tests/70 483/70-843/1.1/UsingAsyncAndAwait.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Exam.Objective1_1
+{
+    class UsingAsyncAndAwait
+    {
+        // Listing 1-18 async and await
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+    }
+}
--- Coding Tests/70 483/70-843/1.1/UsingParallel.cs
+++ Coding Tests/70 483/70-843/1.1/UsingParallel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,12 +7,35 @@ namespace Exam.Objective1_1
 {
     class UsingParallel
     {
+        // Listing 1-16 Using Parallel.For and ForEach
         public static void ParallelFor()
         {
-            Parallel.For(0, 10, i =>
+            Parallel.For(0, 10, (i) =>
             {
+                Console.WriteLine(i);
                 Thread.Sleep(1000);
             });
+
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i =>
+             {
+                 Thread.Sleep(1000);
+             });
+        }
+
+        //Listing 1-17 Using Parallel.Break
+        public static void UsingParallelBreak()
+        {
+            ParallelLoopResult result = Parallel.
+                For(0, 1000, (int i, ParallelLoopState loopState) =>
+                {
+                    if (i == 500)
+                    {
+                        Console.WriteLine("Breaking loop");
+                        loopState.Break();
+                    }
+                    return;
+                });
         }
 
         public static void ParallelBreak()
