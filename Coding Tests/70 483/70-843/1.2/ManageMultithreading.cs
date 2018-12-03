1096fd1a6cedf899a1ad243e6224ba214c618021
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-03 16:06:58 +0000

    Locking and the volatile key word

--- Coding Tests/70 483/70-843/1.2/ManageMultithreading.cs
+++ Coding Tests/70 483/70-843/1.2/ManageMultithreading.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exam.Objective1_2
@@ -58,5 +59,73 @@ namespace Exam.Objective1_2
             myTask.Wait(n);
             Console.WriteLine(n);
         }
+
+        // Listing 1-37 Creating a deadlock
+        public static void CreatingDeadlock()
+        {
+            object lockA = new object();
+            object lockB = new object();
+
+            Task myTask = Task.Run(() =>
+               {
+                   lock (lockA)
+                   {;
+                       lock (lockB)
+                       {
+                           Console.WriteLine("Locked A and B");
+                       }
+                   }
+               });
+            lock (lockB)
+            {
+                Thread.Sleep(1000);
+                lock (lockA)
+                {
+                    Console.WriteLine("Locked B and A");
+                }
+            }
+            myTask.Wait();
+        }
+
+        // Listing 1-38 Generated code from a lock statment
+        public static void GeneratedCodeFromLockStatment()
+        {
+            object gate = new object();
+            bool __lockTaken = false;
+            try
+            {
+                Monitor.Enter(gate, ref __lockTaken);
+            }
+            finally
+            {
+                if (__lockTaken)
+                {
+                    Monitor.Exit(gate);
+                }
+            }
+        }
+
+
+        class Volatile
+        {
+            // Listing 1-39 A potential problem with multithreaded code
+
+            private static int _flag = 0;
+            private static volatile int _value = 0;
+
+            public static void Thread1()
+            {
+                _value = 5;
+                _flag = 1;
+            }
+
+            public static void Thread2()
+            {
+                if (_flag == 1)
+                {
+                    Console.WriteLine(_value);
+                }
+            }
+    }
     }
 }
