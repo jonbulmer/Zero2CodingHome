e69c58f4b9a589eb12aabf12a361adc0c1c6da3e
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-03 17:26:45 +0000

    Added Cancellation Token

---
+++ Coding Tests/70 483/70-843/1.1/CancelingTasks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exam.Objective1_2
+{
+    public static class CancelingTasks
+    {
+
+        // Listing 1-42 Using a CancellationTocken
+        public static void UsingCancellationTocken()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task myTask = Task.Run(() =>
+               {
+                   while (!token.IsCancellationRequested)
+                   {
+                       Console.Write("*");
+                       Thread.Sleep(1000);
+                   }
+               }, token);
+
+            Console.WriteLine("Press enter to stop the task");
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("Press enter to end the program");
+            Console.ReadLine();
+        }
+    }
+}
