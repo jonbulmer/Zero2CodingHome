f506511469669981600fa03a0100441107f261f8
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-04 14:51:42 +0000

    Lamba expressions

--- Coding Tests/70 483/70-843/1.4/UseDelegate.cs
+++ Coding Tests/70 483/70-843/1.4/UseDelegate.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Exam.Objective1_4
 {
     class UseDelegate
-{
+    {
         public delegate int Calculate(int x, int y);
 
         public int Add(int x, int y)
@@ -28,5 +28,17 @@ namespace Exam.Objective1_4
             calc = Multiply;
             Console.WriteLine(calc(3,4));
         }
-}
+    }
+
+    // Listing 1-81 Using the Action delegate
+    class UseActionDelegate {
+        public UseActionDelegate()
+        {
+            Action<int, int> calc = (int x, int y) =>
+            {
+                Console.WriteLine(x + y);
+            };
+            calc(5, 5);
+        }
+    }
 }
---
+++ Coding Tests/70 483/70-843/1.4/UsingLambdaExpressions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective1_4
+{
+    class UsingLambdaExpressions
+    {
+
+    }
+}
