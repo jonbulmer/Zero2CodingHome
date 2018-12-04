826b757e7b467661c3b7b0394ca6cd216df68b78
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-04 14:19:37 +0000

    Delegates

---
+++ Coding Tests/70 483/70-843/1.4/Multicast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective1_4
+{
+    class MulticastClass
+    {
+        public delegate void Del();
+
+        public void MethodOne()
+        {
+            Console.WriteLine("MethodOne");
+        }
+
+        public void MethodTwo()
+        {
+            Console.WriteLine("MethodTwo");
+        }
+
+        public void Multicast()
+        {
+            Del d = MethodOne;
+            d += MethodTwo;
+
+            // I can call this as a function
+            d();
+        }
+    }
+}
---
+++ Coding Tests/70 483/70-843/1.4/UseDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective1_4
+{
+    class UseDelegate
+{
+        public delegate int Calculate(int x, int y);
+
+        public int Add(int x, int y)
+        {
+            return x = y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public UseDelegate()
+        {
+            Calculate calc = Add;
+
+            Console.WriteLine(calc(3, 4));
+
+            // pointing the calc function at muliply
+            calc = Multiply;
+            Console.WriteLine(calc(3,4));
+        }
+}
+}
