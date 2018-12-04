dbbfc51ac3bffa423be6b7602c8188fe1b97193d
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-04 17:05:11 +0000

    Exceptions

---
+++ Coding Tests/70 483/70-843/1.5/HandlingExceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective1_5
+{
+    class HandlingExceptions
+    {
+        //Listing 1-88 Parsing an invailid number
+        public static void ParsingAnInvailidNumber()
+        {
+            string s = "Hello";
+            int i = int.Parse(s);
+        }
+
+        // Listing 1-92 Using Environment.FailFast
+        public static void UsingEnvironmentFailFast()
+        {
+            string s = Console.ReadLine();
+            try
+            {
+                int i = int.Parse(s);
+                if (i == 42)
+                {
+                    Environment.FailFast("Special number entered");
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Program Complete."); ;
+            }
+        }
+
+    }
+}
---
+++ Coding Tests/70 483/70-843/1.5/ThrowExceptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Exam.Objective1_5
+{
+    class ThrowExceptions
+    {
+        //Listing 1-94 Throwing an ArgumentNullException
+        public static string ThrowingAnArgumentNullException(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("fileName", "Filename is required ");
+            }
+            return File.ReadAllText(fileName);
+        }
+
+        // Listing 1-96 Thowinga new exception that points to the original one
+        public static void CallAbove()
+        {
+            try
+            {
+                string s = ThrowingAnArgumentNullException(null);
+            }
+            catch (ArgumentNullException e)
+            {
+
+                Console.WriteLine("Arg null ex: " + e.Message); ;
+            }
+        }
+    }
+}
