1531c568488ba223553e04ed5b8598daccd32150
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-05 13:11:25 +0000

    boxing and unboxing

--- Coding Tests/70 483/70-843/2.2/BoxingAndUnboxing.cs
+++ Coding Tests/70 483/70-843/2.2/BoxingAndUnboxing.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace Exam.Objective2_2
 {
+    // Converting a value type to a reference type
     class BoxingAndUnboxing
     {
+        // Listing 2-19 Boxing an integer value
+        public static void BoxingAnIntegerValue()
+        {
+            int i = 42;
+            object o = i;
+            int x = (int)o;
 
+            string.Concat("To box or not box", 42, true);
+            string s = "To box or not box" + 42 + true;
+        }
     }
 }
