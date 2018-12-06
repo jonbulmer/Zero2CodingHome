ecea9876646f914911df46c7d8054d36ac40f6c6
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-06 11:12:29 +0000

    Using access modifiers and explicit interfaces

---
+++ Coding Tests/70 483/70-843/2.3/UsingAccessModifiers.cs
@@ -0,0 +1,71 @@
+﻿/*
+ Table 2-4 Allowed access modifiers on nested types
+ enum       public      none
+ class      private     all
+ interface  public      none
+ struct     private     public,internal,private 
+
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+[assembly: InternalsVisibleTo("Friend1b")]
+namespace Exam.Objective2_3
+{
+    class UsingAccessModifiers
+    {
+
+    }
+
+    // Listing 2-31 Using access modifiers
+
+    public class Dog
+    {
+        public void Bark() { }
+    }
+
+    // Listing 2-32 Using the private access modifier
+    public class Accessibility
+    {
+        private string _myField;
+        public string MyProperty
+        {
+            get { return _myField; }
+            set { _myField = value; }
+        }
+    }
+
+    // Listing 2-34 Using the protected access modifier with inheritance
+    public class Base
+    {
+        private int _privateField = 42;
+        protected int _protectedField = 42;
+
+        private void MyPrivateMethod() { }
+        protected void MyProtectedMethod() { }
+    }
+
+    public class Derived : Base
+    {
+        public void MyDerivedMethod()
+        {
+            _protectedField = 43;
+
+            MyProtectedMethod();
+        }
+    }
+
+    
+    internal class MyInternalClass
+    {
+        public void MyMethod() { }
+    }
+
+}
+
+
+
---
+++ Coding Tests/70 483/70-843/2.3/UsingProperties.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam.Objective2_3
+{
+    class UsingProperties
+    {
+        // Listing 2-36 Encapsulating a field with custom metods
+        private int _field;
+        public void SetValue(int value) { _field = value; }
+
+        public int GetValue() { return _field; }
+    }
+
+    // Listing 2-37 Creating a property
+    class Person
+    {
+        private string _firstname;
+        public string Firstname
+        {
+            get { return _firstname; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException();
+                }
+                _firstname = value;
+            }
+        }
+
+        public string Surname { get; set; }
+    }
+
+    class UsingExplcitInterfaceImplementations
+    {
+        // Listing 2-38 The IOjectContextAdapter
+        public interface IOjectContextAdapter
+        {
+            ObjectContext ObjectContext { get; }
+        }
+    }
+
+    public class ObjectContext
+    {
+    }
+
+    // Listing 2-39 Implementing an interface explictly
+    interface IInterfaceA
+    {
+        void MyMethod();
+    }
+
+    class Implementation : IInterfaceA
+    {
+        void IInterfaceA.MyMethod() { }
+    }
+
+    // Listing 2-40 Implementing an interface explicitly
+    interface ILeft
+    {
+        void Move();
+    }
+
+    interface IRight
+    {
+        void Move();
+    }
+
+    class MoveableObject : ILeft, IRight
+    {
+        void ILeft.Move() { }
+        void IRight.Move() { }
+    }
+
+}
