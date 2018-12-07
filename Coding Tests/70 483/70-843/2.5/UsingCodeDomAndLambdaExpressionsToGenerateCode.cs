d4096a2682deb29be34adf843200242171a62a07
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-07 10:39:22 +0000

    Using  CodeDom

---
+++ Coding Tests/70 483/70-843/2.5/UsingCodeDomAndLambdaExpressionsToGenerateCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace Exam.Objective2_5
+{
+    // Listing 2-74 Generating "Hello World!" with the CodeDOM
+    class Sample
+    {
+
+        private const string outputFileName = "SampleCode.cs";
+        static void Main(string[] args)
+        {
+        }
+    }
+    //CodeCompliteUnit = new
+
+    // Listing 2-75 Generating a source file from a CodeComplieUnit 
+}
