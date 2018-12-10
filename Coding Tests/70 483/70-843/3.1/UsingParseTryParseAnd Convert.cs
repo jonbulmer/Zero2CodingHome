4dceb0cf0edcae7c1d9eb5f0a2b5abd6e77c69a3
Jon Bulmer <jon.bulmer@gmail.com>
2018-12-10 14:59:46 +0000

    Work with chapter 3

---
+++ Coding Tests/70 483/70-843/3.1/UsingParseTryParseAnd Convert.cs
@@ -0,0 +1,55 @@
+﻿#define MySymbol
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Text;
+
+namespace Exam.Objective3_1
+{
+    public class UsingParseTryParseAndConvert
+    {
+        // Listing 3-4 Using Parse
+        public static void UsingParse()
+        {
+            string value = "true";
+            bool b = bool.Parse(value);
+            Console.WriteLine(b);
+        }
+
+        //Listing 3-5 Using TryParse
+        public static void UsingTryPase()
+        {
+            string value = "1";
+            int result;
+            bool success = int.TryParse(value, out result);
+            if (success)
+            {
+                Console.WriteLine($"Value is a valid Integer {value}");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+
+        // Listing 3-6 Using configuration options when parsing a number
+        public static void UsingConfigurationOptionsWhenParsingNumber()
+        {
+            CultureInfo english = new CultureInfo("En");
+            CultureInfo dutch = new CultureInfo("Nl");
+
+            string value = "$19,50";
+            decimal d = decimal.Parse(value, NumberStyles.Currency, dutch);
+            Console.WriteLine(d.ToString(english));
+        }
+
+        //Listing 3-8 Using Convert to convert from double to int
+        public static void UsingConvertToConvertFromDoubleToInt()
+        {
+            double d = 23.45;
+            int i = Convert.ToInt32(d);
+            Console.WriteLine($"i is now an int with a value of : {i}");
+        }
+    }
+}
---
+++ Coding Tests/70 483/70-843/3.1/WhyValidatingApplictionInputIsImportant.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Text;
+
+namespace Exam.Objective3_1
+{
+    // Listing 3-1 Customer and Addresses classes
+    class ManagingDataIntegrity
+    {
+        public class Customer
+        {
+            public int Id { get; set; }
+
+            [Required, MaxLength(20)]
+            public string Firstname { get; set; }
+
+            [Required, MaxLength(20)]
+            public string LastName { get; set; }
+
+            [Required]
+            public Address ShippingAdderss { get; set; }
+
+            [Required]
+            public Address BillingAddress { get; set; }
+        }
+
+        public class Address
+        {
+            public int Id { get; set; }
+
+            [Required, MaxLength(20)]
+            public string  AddressLine1 { get; set; }
+
+            [Required, MaxLength(20)]
+            public string AddressLine2 { get; set; }
+
+            [Required, MaxLength(20)]
+            public string City { get; set; }
+
+            [RegularExpression(@"^[A-Z]")]
+            public string Postcode { get; set; }
+        }
+    }
+
+    // Listing 3-2 Saving a new customer to the database
+
+
+
+    // Listing 3-3 Running manual validation
+    public static class GenericValidator<T>
+    {
+        public static IList<ValidationResult> Validate(T entity)
+        {
+            var result = new List<ValidationResult>();
+            var context = new ValidationContext(entity, null, null);
+            Validator.TryValidateObject(entity, context, result);
+            return result;
+        }
+    }
+
+}
