a67637b461a1d5b03a921c8d2bda4e24a1b82d2f
Jon Bulmer <jon.bulmer@gmail.com>
2019-01-21 21:27:16 +0000

    getting strated with the train ride

---
+++ Coding Tests/TrainRideDemo/TrainRideDemo/TrainRideDemo.UI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainRideDemo.UI
+{
+    public class Class1
+    {
+    }
+}
