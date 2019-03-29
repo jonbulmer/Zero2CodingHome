96532297d8864066c4564ab468207fb36608ba5f
Jon Bulmer <jon.bulmer@gmail.com>
2019-03-29 08:29:09 +0000

    Working out some basics around DbMocks

--- Coding Tests/TrainRideDemo/TrainRideDemo/TrainRideDemo.Tests/ViewModel/LiveTrains/LiveTrainsViewModelTests.cs
+++ Coding Tests/TrainRideDemo/TrainRideDemo/TrainRideDemo.Tests/ViewModel/LiveTrains/LiveTrainsViewModelTests.cs
@@ -21,14 +21,16 @@ namespace TrainRideDemo.Tests
             var mockSetStopMaxtrixRideDbSet = Substitute.For<DbSet<StopMaxtrix>>();
             var MockTrainRideContext = Substitute.For<IStopMaxtrixDbContext>();
             MockTrainRideContext.StopMaxtrixs.Returns(mockSetStopMaxtrixRideDbSet);
+            mockSetStopMaxtrixRideDbSet.Add(new StopMaxtrix { Id = 1, PositionIndex = 1, RouteId = 1, StopProximityId = 1 });
             var service = new LiveTrainsService();
             //Act
 
-            service.Add(new LiveTrainsViewModel { TrainName= "Test" , RouteName = "Big Test"  });
+            //service.Add(new LiveTrainsViewModel { TrainName= "Test" , RouteName = "Big Test"  });
 
             // Assert
 
             mockSetStopMaxtrixRideDbSet.Received(1).Add(Arg.Any<StopMaxtrix>());
+           
         }
 
 
